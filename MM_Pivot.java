fbf0384c6860c13354265eb3eece08c784bbd48c
lincoln12833 <Lincolnbruning@protonmail.com>
2024-11-02 13:51:59 -0400

    Mostly add incremental movement

--- MM_Pivot.java
+++ MM_Pivot.java
@@ -1,35 +1,86 @@
 package org.firstinspires.ftc.teamcode.MM;
 
+import static org.firstinspires.ftc.teamcode.MM.MM_Collector.haveSample;
+
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class MM_Pivot {
+    private final MM_OpMode opMode;
+
     private DcMotorEx pivot = null;
 
-    private Rev2mDistanceSensor topLimit;
+    private TouchSensor bottomLimit;
 
-    private  Rev2mDistanceSensor bottomLimit;
+    private boolean homing = false;
 
-    private final MM_OpMode opMode;
+    public final int TICK_INCREMENT = 10;
+
+    private final int MAX_HEIGHT = 2000;;
+
+
+    public int targetPos = 0;
+
+
 
     MM_Pivot(MM_OpMode opMode){
         this.opMode = opMode;
+        init();
+
     }
 
     public void controlPivot(){
+        opMode.telemetry.addData("Current pos", pivot.getCurrentPosition());
+        opMode.telemetry.addData("target pos", pivot.getTargetPosition());
+        opMode.telemetry.addData("Current current", pivot.getCurrent(CurrentUnit.AMPS));
+        opMode.telemetry.addData("Current velocity", pivot.getVelocity());
+        opMode.telemetry.addData("PID Coefficients", pivot.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION));
+        opMode.telemetry.addData("target position tolerance", pivot.getTargetPositionTolerance());
+        opMode.telemetry.addData("current alert", pivot.getCurrentAlert(CurrentUnit.AMPS));
+        opMode.telemetry.addData("is over current =",  pivot.isOverCurrent());
 
+        opMode.telemetry.update();
+
+        if (opMode.gamepad1.x && !bottomLimit.isPressed()){
+            pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            pivot.setPower(-.7);
+            homing = true;
+        }
+
+        if(bottomLimit.isPressed() || Math.abs(opMode.gamepad1.left_stick_y) > 0.1){
+            homing = false;
+            pivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            pivot.setPower(1);
+        }
+
+        if (-opMode.gamepad1.left_stick_y > .1){
+            targetPos = Math.min(targetPos + TICK_INCREMENT, MAX_HEIGHT);
+        } else if (-opMode.gamepad1.left_stick_y < -.1){
+            targetPos = Math.max(targetPos - TICK_INCREMENT, 0);
+        }
+
+        if (!homing) {
+            pivot.setTargetPosition(targetPos);
+        }
     }
 
     public void init(){
         pivot = opMode.hardwareMap.get(DcMotorEx.class, "pivot");
 
-        pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        pivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        pivot.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        pivot.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        pivot.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         pivot.setTargetPosition(0);
+        pivot.setPower(1);
 
-        topLimit = opMode.hardwareMap.get(Rev2mDistanceSensor.class, "topLimit");
-        bottomLimit = opMode.hardwareMap.get(Rev2mDistanceSensor.class, "bottomLimit");
+
+
+        bottomLimit = opMode.hardwareMap.get(TouchSensor.class, "bottomLimit");
     }
 
 }
