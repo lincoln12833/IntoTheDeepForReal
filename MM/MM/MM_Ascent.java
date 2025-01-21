16b39bafc0418ddde08e80c0c4d8520bc7079464
lincoln12833 <Lincolnbruning@protonmail.com>
2025-01-21 10:32:32 -0500

    Fix Git on lincoln's PC

---
+++ MM/MM/MM_Ascent.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.MM.MM;
+import org.firstinspires.ftc.teamcode.MM.MM.MM_OpMode;
+public class MM_Ascent {
+    private MM_OpMode opMode;
+    public MM_Ascent(MM_OpMode opMode){
+        this.opMode = opMode;
+    }
+
+
+    public void init(){
+
+    }
+}
